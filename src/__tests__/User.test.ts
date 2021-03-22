663ac6a07e40a2baddfac816fa3a6a6df26ac0b3
jefferson.souza <jefferson.souza@synchro.com.br>
2021-03-22 12:25:21 -0300

    setting up jest and tests

---
+++ src/__tests__/User.test.ts
@@ -0,0 +1,29 @@
+import request from 'supertest';
+
+import app from '../app';
+import createConnection from '../db';
+
+describe('User', () => {
+  beforeAll(async () => {
+    const conn = await createConnection();
+    await conn.runMigrations();
+  });
+
+  it('Should be able to create a new user', async () => {
+    const response = await request(app).post('/users').send({
+      email: 'user@example.com',
+      name: 'Example',
+    });
+
+    expect(response.status).toBe(201);
+  });
+
+  it('Should not be able to create a user with the an existing email', async () => {
+    const response = await request(app).post('/users').send({
+      email: 'user@example.com',
+      name: 'Example',
+    });
+
+    expect(response.status).toBe(400);
+  });
+});
